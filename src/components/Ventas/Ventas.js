2c6890729693483810b790ab7416b5100d3c6e9b
jproggar <jorgegarcia72@hotmail.com>
2021-11-14 21:05:35 -0500

    revision front8

--- src/components/Ventas/Ventas.js
+++ src/components/Ventas/Ventas.js
@@ -18,6 +18,7 @@ import {
 } from "reactstrap";
 import Tablero from '../Tablero';
 
+
 const data = [];
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const PATH_SALES = process.env.REACT_APP_API_SALES_PATH;
